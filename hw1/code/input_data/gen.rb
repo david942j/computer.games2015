540aff879e2fcb4b20ab9ca01267ba7ab81dc2d5
david942j <david942j@gmail.com>
2015-10-12 01:04:48 +0800

    tolerant

--- hw1/code/input_data/gen.rb
+++ hw1/code/input_data/gen.rb
@@ -1,7 +1,7 @@
 
 def main(size=20)
   s=%x(curl http://cn.puzzle-nurikabe.com?size=4)
-  ar = s.scan(/<td >[0-9]*<\/td>|<td><img class="l" name="i[_\d]*" src="nun.gif" \/><\/td>/).map{|c| next '.' if c.match(/img/); next c.scan(/\d+/).first }
+  ar = s.scan(/<td\s?>[0-9]+<\/td>|<td><img class="l" name="i[_\d]+" src="nun.gif"\s?\/?><\/td>/).map{|c| next '.' if c.match(/img/); next c.scan(/\d+/).first }
   puts size
   puts ar.join.scan(/.{#{size}}/)
   puts "id: "+s.scan(/<p>20x20 数墙 题号: ([\d,]+)<\/p>/)[0][0].split(',').join
